fbeb138621ff259383c53740b0f1ca438d43fe15
João Rafael Fagundes Menezes <menezes.jrafael@gmail.com>
2020-05-14 22:10:45 -0300

    Adicionar arquivos de projeto.

---
+++ EduardoPiresMinhaDemoMVCCore/ViewComponents/CarrinhoViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduardoPiresMinhaDemoMVCCore.ViewComponents
+{
+    //Chamada
+    [ViewComponent(Name = "Carrinho")]
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        public int ItemCarrinho { get; set; }
+
+        public CarrinhoViewComponent()
+        {
+            ItemCarrinho = 3;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(ItemCarrinho);
+        }
+    }
+}
